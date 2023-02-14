ed7385807e1414c607164dd24dd7cdbed66f3a4d
leomejiac <joseleo31@hotmail.com>
2023-02-14 10:57:55 -0500

    [leo]: creación de cliente
    se crea la interfaz cliente

---
+++ frontend/src/app/cliente/cliente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente',
+  templateUrl: './cliente.component.html',
+  styleUrls: ['./cliente.component.css']
+})
+export class ClienteComponent {
+
+}
