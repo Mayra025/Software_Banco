47a03460615a55673fc69880b4c12897e6d078e0
Mayra025 <maypachacama@hotmail.com>
2023-02-21 12:58:59 -0500

    [Mayra025]: logout

--- frontend/src/app/empleado/empleado.component.ts
+++ frontend/src/app/empleado/empleado.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output  } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import axios from 'axios';
 
@@ -8,30 +8,43 @@ import axios from 'axios';
   styleUrls: ['./empleado.component.css']
 })
 export class EmpleadoComponent implements OnInit {
-  option:number =0;
-  public data:any;
+  option: number = 0;
+  public data: any;
 
 
 
- activarComponente(@Output() opcion:number ){
-   this.option = opcion;
- }
+  activarComponente(@Output() opcion: number) {
+    this.option = opcion;
+  }
 
-/*
-  constructor(private _router:Router) { 
-    axios.get("http://localhost:8080/api/empleado/info", {withCredentials: true}).then(resp => {
+
+  constructor(private _router: Router) {
+    /*axios.get("http://localhost:8080/api/empleado/info", { withCredentials: true }).then(resp => {
       this.data = resp.data;
       console.log(this.data);
-      
+
     }).catch(err => {
       this._router.navigate(['/empleado-login']);
-    })
+    })*/
 
   }
-  */
-  
+
+
 
   ngOnInit(): void {
+  }
+
+  logout(): void {
+    this._router.navigate(['/empleado-login']);
+
+    /*
+    axios.post("http://localhost:8080/api/logout", {}, { withCredentials: true }).then(resp => {
+      window.location.reload();
+    }).catch(err => {
+      console.log(err);
+
+    })
+*/
   }
 
 }
