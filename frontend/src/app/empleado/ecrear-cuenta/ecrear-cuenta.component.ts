1a433a253e0ace06fcd3f7ddba7b78b12c870d14
hashidk <wandres_mv1@hotmail.com>
2023-02-27 12:49:01 -0500

    Ahora se pueden asociar más clientes a una cuenta

--- frontend/src/app/empleado/ecrear-cuenta/ecrear-cuenta.component.ts
+++ frontend/src/app/empleado/ecrear-cuenta/ecrear-cuenta.component.ts
@@ -1,5 +1,5 @@
 //Empleado crea Cuenta
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Renderer2  } from '@angular/core';
 import { CuentaB } from '../../models/cuenta';
 import axios from 'axios';
 import { Router } from '@angular/router';
@@ -17,17 +17,20 @@ export class EcrearCuentaComponent implements OnInit {
   @Input() objR: string;
   public cta: CuentaB;
   public clientes: any;
+  public clientesFilter: any;
 
   opcion: number = 11;
   public error: string = "";
   public success: string = "";
+  public selectedCli:string[] = []
 
 
-  constructor(private _router: Router, private _EmpleadoService: EmpleadoService) {
+  constructor(private _router: Router, private _EmpleadoService: EmpleadoService, private renderer: Renderer2) {
     this.cta = new CuentaB(null, '', '', 0, false)
 
     this._EmpleadoService.getClientes().subscribe(resp=>{
       this.clientes = resp.data;
+      this.clientesFilter = resp.data;
     },err => {
       this._router.navigate(['/login']);
     })
@@ -40,7 +43,7 @@ export class EcrearCuentaComponent implements OnInit {
   onSubmit(formCta: NgForm) {
 
     this._EmpleadoService.addCuenta({
-      clientes: formCta.value.id,
+      clientes: this.selectedCli,
       tipo: formCta.value.tipo
     }).subscribe(resp=>{
       this.error = "";
@@ -52,5 +55,17 @@ export class EcrearCuentaComponent implements OnInit {
 
   }
 
+  onChange(deviceValue) {
+    if (deviceValue.target.value !== "none") {
+      this.selectedCli.push(deviceValue.target.value)
+      this.clientesFilter = this.clientes.filter(ele => !this.selectedCli.includes(ele._id))
+    }
+  }
+
+  
+  handleclick(id:string){
+    this.selectedCli = this.selectedCli.filter(ele => ele !== id)
+    this.clientesFilter = this.clientes.filter(ele => !this.selectedCli.includes(ele._id))
+  }
 }
 
